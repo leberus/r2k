a5068874d5979c16c38db7ab18e29d90c88d25d5
leberus <osalvador.vilardaga@gmail.com>
2016-09-07 18:07:21 +0200

    r/w kernel memory implement. initial r from linear address prepared

--- r2k.c
+++ r2k.c
@@ -3,20 +3,48 @@
 #include <linux/ioctl.h>
 #include <linux/cdev.h>
 #include <linux/fs.h>
+#include <linux/page-flags.h>
+#include <linux/mm.h>
+#include <linux/slab.h>
+#include <linux/sched.h>
+#include <linux/pagemap.h>
 #include <asm/uaccess.h>
+#include <asm/highmem.h>
+
+#define PTE_CHECK_BIT(x)	(_AT(pteval_t, 1) << x)
+
+/*
+	search how the modules are being loaded
+*/
+
+
+/*
+
+	static inline int pte_write(pte_t pte) { return pte_val(pte) & _PAGE_WRITABLE; }
+
+*/
 
 static char R2_TYPE = 'k';
 
-#define READ_KERNEL_MEMORY	0x1
-#define WRITE_KERNEL_MEMORY	0x2
+#define IOCTL_READ_KERNEL_MEMORY	0x1
+#define IOCTL_WRITE_KERNEL_MEMORY	0x2
+#define IOCTL_READ_LINEAR_ADDR		0x3
+#define IOCTL_WRITE_LINEAR_ADDR		0x4
+#define IOCTL_READ_PHYSICAL_ADDR	0x5
+#define IOCTL_WRITE_PHYSICAL_ADDR	0x6
+#define IOCTL_GET_PROC_MAPS		0x7
+#define IOCTL_GET_KERNEL_MAP		0x8
+
 
 static struct cdev *r2_dev;
 static dev_t dev;
 static char *r2_devname = "r2";
 
+static unsigned char c = 'a';
 
 struct r2k_data {
-	unsigned long *addr;
+	int pid;
+	unsigned long addr;
 	unsigned long len;
 	void __user *buff;
 };
@@ -31,11 +59,52 @@ static int io_close (struct inode *inode, struct file *file)
 	return 0;
 }
 
+static unsigned int addr_is_mapped (unsigned long addr)
+{
+	pte_t *pte;
+	unsigned int level;
+
+	pte = lookup_address (addr, &level);
+	if (pte) {
+		return (pte_val (*pte) & _PAGE_PRESENT);
+	}
+
+	pr_info ("%s: addr_is_mapped - pte_null\n", r2_devname);
+	return 0;
+}
+
+static unsigned int addr_is_writeable (unsigned long addr)
+{
+	pte_t *pte;
+	unsigned int level;
+
+	pte = lookup_address (addr, &level);
+	if (pte) {
+		return (pte_val (*pte) & _PAGE_RW);
+	}
+
+	pr_info ("%s: pte == null\n", r2_devname);
+	return 0;
+}
+
+static int check_kernel_addr (unsigned long addr)
+{
+	return virt_addr_valid (addr) == 0 ? is_vmalloc_addr ((void *)addr) : 1;
+}
+
+static void check_vmalloc_addr (unsigned long addr)
+{
+	if (is_vmalloc_addr ((void *)addr))
+		pr_info ("%s: 0x%lx belongs to vmalloc\n", r2_devname, addr);
+	else
+		pr_info ("%s: 0x%lx does not belong to vmalloc\n", r2_devname, addr);
+}
+
 static long io_ioctl (struct file *file, unsigned int cmd, unsigned long data_addr)
 {
 	struct r2k_data __user *data = (struct r2k_data __user *)data_addr;
 	unsigned long len;
-	int ret;
+	int ret = 0;
 
 	if (_IOC_TYPE (cmd) != R2_TYPE)
 		return -EINVAL;
@@ -44,26 +113,107 @@ static long io_ioctl (struct file *file, unsigned int cmd, unsigned long data_ad
 
 	switch (_IOC_NR (cmd)) {
 
-	case READ_KERNEL_MEMORY:
-
-		if (data->addr < PAGE_OFFSET) {
-			pr_info ("%s: error - 0x%lx belongs to USERSPACE\n", r2_devname, (unsigned long)data->addr);
-			ret = -EINVAL;
+	case IOCTL_READ_KERNEL_MEMORY:
+		
+		pr_info ("%s: IOCTL_READ_KERNEL_MEMORY on 0x%lx\n", r2_devname, data->addr);
+	
+		if (!check_kernel_addr (data->addr)) {
+			pr_info ("%s: 0x%lx invalid addr\n", r2_devname, data->addr);
+			ret = -EFAULT;
 			return ret;
-		} else {
-			pr_info ("%s: 0x%lx above PAGE_OFFSET\n", r2_devname, (unsigned long)data->addr);
 		}
 
-		pr_info ("%s: addr: 0x%lx  val: 0x%lx\n", r2_devname, (unsigned long)data->addr, (unsigned long)*(data->addr));
+		check_vmalloc_addr (data->addr);
+
+		if (!addr_is_mapped (data->addr)) {
+			pr_info ("%s: addr is not mapped\n", r2_devname);
+			ret = -EFAULT;
+			return ret;
+		}
 
 		ret = copy_to_user (data->buff, (void *)data->addr, len);
 		if (ret) {
-			pr_info ("error: copy_to_user failed\n");
-			ret = -EINVAL;
+			pr_info ("%s: copy_to_user failed\n", r2_devname);
+			ret = -EFAULT;
 			return ret;
 		}
 
 		break;
+
+	case IOCTL_WRITE_KERNEL_MEMORY:
+
+		pr_info ("%s: IOCTL_WRITE_KERNEL_MEMORY on 0x%lx\n", r2_devname, data->addr);
+
+		if (!check_kernel_addr (data->addr)) {
+			pr_info ("%s: 0x%lx invalid addr\n", r2_devname, data->addr);
+			ret = -EFAULT;
+			return ret;
+		}	
+
+		if (!addr_is_writeable (data->addr)) {
+			pr_info ("%s: cannot write at addr 0x%lx\n", r2_devname, data->addr);
+			ret = -EPERM;
+			return ret;
+		}
+				
+		ret = copy_from_user ((void *)data->addr, data->buff, len);
+		if (ret) {
+			pr_info ("%s: copy_to_user failed\n", r2_devname);
+			ret = -EFAULT;
+			return ret;
+		}
+				
+		break; 
+
+	case IOCTL_READ_LINEAR_ADDR:
+		pr_info ("%s: IOCTL_READ_LINEAR_ADDR on 0x%lx from pid, %d bytes (%ld)\n", r2_devname, data->addr, data->pid, data->len);
+
+		struct page *pg;
+		struct task_struct *task;
+		struct vm_area_struct *vma;
+		void *kaddr;
+		
+		task = pid_task (find_vpid (data->pid), PIDTYPE_PID);
+		if (!task) {
+			pr_info ("%s: could not retrieve task_struct from pid (%d)\n", r2_devname, data->pid);
+			ret = -ESRCH;
+			return ret;
+		}
+
+		vma = find_vma (task->mm, data->addr);
+		if (vma) {
+			pr_info ("%s: vma->addr - 0x%lx\n", r2_devname, vma->vm_start);
+		}
+
+		down_read (&task->mm->mmap_sem);
+		ret = get_user_pages (task, task->mm, data->addr, 1, 0, 0, &pg, NULL);
+		if (!ret) {
+			pr_info ("%s: could not retrieve page from pid (%d)\n", r2_devname, data->pid);
+			ret = -ESRCH;
+			return ret;
+		}
+
+		kaddr = kmap (pg);
+		if (!kaddr) {
+			pr_info ("%s: could not map the page from pid (%d)\n", r2_devname, data->pid);
+			up_read (&task->mm->mmap_sem);
+			ret = -ESRCH;
+			return ret;
+		}
+
+		pr_info ("%s: kaddr : 0x%p\n", r2_devname, kaddr);
+		ret = copy_to_user (data->buff, kaddr + (data->addr - vma->vm_start), len);
+		if (ret) {
+			up_read (&task->mm->mmap_sem);
+			pr_info ("%s: copy_to_user failed\n", r2_devname);
+			ret = -EFAULT;
+			return ret;
+		}
+	
+		page_cache_release (pg);
+		up_read (&task->mm->mmap_sem);		
+		break;
+
 	default:
 		pr_info ("%s: operation not implemented\n", r2_devname);
 		ret = -EINVAL;
@@ -80,6 +230,7 @@ static struct file_operations fops = {
         .unlocked_ioctl = io_ioctl,
 };
 
+
 static int __init r2k_init (void)
 {
         int ret;
@@ -92,7 +243,16 @@ static int __init r2k_init (void)
 	cdev_add (r2_dev, dev, 1);
 	
 	pr_info ("%s: please create the proper device with - mknod /dev/%s c %d %d\n", r2_devname, r2_devname, MAJOR (dev), MINOR (dev));
-	
+	pr_info ("%s: VMALLOC_END:\t\t0x%lx\n", r2_devname, VMALLOC_END);
+	pr_info ("%s: VMALLOC_OFFSET:\t\t0x%x\n", r2_devname, VMALLOC_OFFSET);
+	pr_info ("%s: PKMAP_BASE:\t\t0x%lx\n", r2_devname, PKMAP_BASE);
+	pr_info ("%s: FIXADDR_START:\t\t0x%lx\n", r2_devname, FIXADDR_START);
+	pr_info ("%s: FIXADDR_TOP:\t\t0x%lx\n", r2_devname, FIXADDR_TOP);
+	pr_info ("%s: mem_map:\t\t\t0x%p\n", r2_devname, &mem_map);
+	pr_info ("%s: __START_KERNEL_map:\t0x%lx\n", r2_devname, __START_KERNEL_map);
+	pr_info ("%s: KERNEL_IMAGE_SIZE:\t\t0x%x\n", r2_devname, KERNEL_IMAGE_SIZE);
+	pr_info ("%s: c variable memory:\t\t0x%p\n", r2_devname, &c);
+
 	return 0;
 }
 
@@ -100,10 +260,12 @@ static void __exit r2k_exit (void)
 {
 	unregister_chrdev_region (dev, 1);
 	cdev_del (r2_dev);
-	pr_info ("%s: unloading driver\n", r2_devname);
+	pr_info ("%s: unloading driver, please delete /dev/%s\n", r2_devname, r2_devname);
 }
 
 module_init (r2k_init);
 module_exit (r2k_exit);
 
-MODULE_LICENSE("GPL v3");
+MODULE_AUTHOR("Oscar Salvador");
+MODULE_DESCRIPTION("r2k");
+MODULE_LICENSE("GPL v2");
--- r2k_u.c
+++ r2k_u.c
@@ -4,18 +4,60 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <errno.h>
 
+#define CHAR_FMT "%c "
+#define HEX_FMT "0x%02x "
+
+#define HEX 	0x0
+#define CHAR 	0x1
 
 struct r2k_data {
-        unsigned long *addr;
+	int pid;
+        unsigned long addr;
         unsigned long len;
-        unsigned long *buff;
+        unsigned char *buff;
 };
 
 static char R2_TYPE = 'k';
 
-#define READ_KERNEL_MEMORY	_IOR (R2_TYPE, 0x1, sizeof (struct r2k_data))
-#define WRITE_KERNEL_MEMORY	_IOR (R2_TYPE, 0x2, sizeof (struct r2k_data))
+#define IOCTL_READ_KERNEL_MEMORY	_IOR (R2_TYPE, 0x1, sizeof (struct r2k_data))
+#define IOCTL_WRITE_KERNEL_MEMORY	_IOR (R2_TYPE, 0x2, sizeof (struct r2k_data))
+#define IOCTL_READ_LINEAR_ADDR          _IOR (R2_TYPE, 0x3, sizeof (struct r2k_data))
+#define IOCTL_WRITE_LINEAR_ADDR         _IOR (R2_TYPE, 0x4, sizeof (struct r2k_data))
+#define IOCTL_READ_PHYSICAL_ADDR        _IOR (R2_TYPE, 0x5, sizeof (struct r2k_data))
+#define IOCTL_WRITE_PHYSICAL_ADDR       _IOR (R2_TYPE, 0x6, sizeof (struct r2k_data))
+#define IOCTL_GET_PROC_MAPS             _IOR (R2_TYPE, 0x7, sizeof (struct r2k_data))
+#define IOCTL_GET_KERNEL_MAP            _IOR (R2_TYPE, 0x8, sizeof (struct r2k_data))
+
+#define READ_KERNEL_MEMORY		0x1
+#define WRITE_KERNEL_MEMORY		0x2
+#define READ_LINEAR_ADDR		0x3
+#define WRITE_LINEAR_ADDR		0x4
+#define READ_PHYSICAL_ADDR		0x5
+#define WRITE_PHYSICAL_ADDR		0x6
+#define GET_PROC_MAPS			0x7
+#define GET_KERNEL_MAP			0x8
+
+const char *ioctl_str[] = 	{
+					"IOCTL_READ_KERNEL_MEMORY",
+					"IOCTL_WRITE_KERNEL_MEMORY",
+					"IOCTL_READ_LINEAR_ADDR",
+					"IOCTL_WRITE_LINEAR_ADDR",
+					"IOCTL_READ_PHYSICAL_ADDR",
+					"IOCTL_WRITE_PHYSICAL_ADDR",
+					"IOCTL_GET_PROC_MAPS",
+					"IOCTL_GET_KERNEL_MAP"
+				};
+
+const char *devicename = "/dev/r2";
+const char *prog_name;
+
+void print_help(void)
+{
+	printf ("%s -a [addr] -i [ioctl] -b [n_bytes] -w [w_bytes] -p [pid]\n", prog_name);
+	exit (-1);
+}
 
 
 int main(int argc, char **argv)
@@ -23,38 +65,120 @@ int main(int argc, char **argv)
 
 	int fd;
 	int ret;
-	unsigned int ioctl_n;
-	char *devicename = "/dev/r2";
-	struct r2k_data data;
-	unsigned long *p;
-	int n_words;
 	int i;
+	int opt;
+	int n_bytes;
+	int n_ioctl;
+	unsigned int ioctl_n;
+	struct r2k_data data;
+	unsigned char c = 98;
+	char *p;
+	int output;
 
-	n_words = atoi (argv[1]);
+	data.addr = data.pid = data.len = 0;
+	prog_name = argv[0];
 
-	data.buff = malloc (n_words * sizeof (unsigned long));
+	if (argc < 4)
+		print_help();
 
-	fd = open (devicename, O_RDONLY);
-	if( fd == -1 ) {
-		perror ("error\n");
-		return -1;
+	while ((opt = getopt (argc, argv, "a:i:b:w:p:o:")) != -1) {
+		switch (opt) {
+		case 'a':
+			data.addr = strtoul (optarg, &p, 16);
+			break;
+		case 'i':
+			n_ioctl = atoi (optarg);
+			break;
+		case 'b':
+			n_bytes = atoi (optarg);
+			break;
+		case 'w':
+			data.buff = optarg;
+		case 'p':
+			data.pid = atoi (optarg);
+			break;
+		case 'o':
+			output = *optarg == 'h'  ? HEX : CHAR;
+		default:
+			printf ("%s: arg %s not valid\n", prog_name, optarg);
+			break;
+		}
 	}
 
-	data.addr = 0xc1002bd0;
-	data.len = sizeof (unsigned long) * n_words;
-	printf ("Number of words to take: %d\n", data.len / (sizeof(void *)));
+	printf ("ioctl: %s\n", ioctl_str[n_ioctl - 1]);
+	printf ("ioctl: addr 0x%lx\n", data.addr);
+	printf ("ioctl: %d bytes\n", n_bytes);
 	
-	ioctl_n = WRITE_KERNEL_MEMORY;
-	//ioctl_n = READ_KERNEL_MEMORY;
-	ret = ioctl (fd, ioctl_n, &data);
+	if (data.pid)
+		printf ("ioctl: pid (%d)\n", data.pid);
 
-	printf ("Got the state: addr: 0x%lx - value: ", data.addr);
-	for (i = 0; i < n_words; i++) {
-		printf ("0x%08lx ", data.buff[i]);
+	fd = open (devicename, O_RDONLY);
+        if( fd == -1 ) {
+                perror ("error\n");
+                return -1;
+        }
+
+	
+
+	switch (n_ioctl) {
+	case READ_KERNEL_MEMORY:
+
+		data.buff = (unsigned char *)calloc (n_bytes, 1);
+		data.len = n_bytes;
+
+		ioctl_n = IOCTL_READ_KERNEL_MEMORY;
+		ret = ioctl (fd, ioctl_n, &data);
+
+		printf ("ret: %d\n", ret);
+		fprintf (stderr, "ioctl err: %s\n", strerror (errno));
+
+		if (!ret) {
+			printf ("Got the state: addr: 0x%lx - value: ", data.addr);
+			for (i = 0; i < n_bytes; i++) {
+				printf (output == HEX ? HEX_FMT : CHAR_FMT , data.buff[i]);
+			}
+		}
+
+		break;
+
+	case WRITE_KERNEL_MEMORY:
+
+		data.buff = &c;
+		data.len = 1;
+
+		printf ("data.buff: %x\n", *data.buff);	
+		
+		ioctl_n = IOCTL_WRITE_KERNEL_MEMORY;
+		ret = ioctl (fd, ioctl_n, &data); 
+		break;
+
+	case READ_LINEAR_ADDR:
+
+		data.buff = (unsigned char *)calloc (n_bytes, 1);
+		data.len = n_bytes;
+
+		printf ("Reading %d bytes at 0x%lx from pid (%d)\n", data.len, data.addr, data.pid);
+
+		ioctl_n = IOCTL_READ_LINEAR_ADDR;
+		ret = ioctl (fd, ioctl_n, &data);
+		printf ("ret: %d\n", ret);
+		fprintf (stderr, "ioctl err: %s\n", strerror (errno));
+
+		if (!ret) {
+			printf ("Got the state: addr: 0x%lx - value: ", data.addr);
+			for (i = 0; i < n_bytes; i++) {
+				printf (output == HEX ? HEX_FMT : CHAR_FMT, data.buff[i]);
+			}
+		}
+
+		break;
+		
+	default:
+		printf ("ioctl not implemented\n");
+		break;
 	}
 
 	printf ("\n");
-
 	close (fd);
 	
 	return 0;
